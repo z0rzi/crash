5d1cf953f011d38926821e6439d641c948c01ec4
Zorzi <baptiste.zorzi@gmail.com>
2020-09-24 18:10:33 +0200

    starting angular crash course

---
+++ 200924-Angular/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'todo-list';
+  name:string = 'Baptiste';
+}
---
+++ 200924-Angular/src/app/components/todos/todos.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Todo 1',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Todo 2',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Todo 3',
+        completed: true
+      },
+      {
+        id: 4,
+        title: 'Todo 4',
+        completed: false
+      }
+    ]
+  }
+}
